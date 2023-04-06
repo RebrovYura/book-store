f32adcc60bac7570433928c4db8264bb300d4efa
Yuri Rebrov <yurirebrov01@gmail.com>
2023-04-06 13:34:22 +0300

    mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
+import './mobileMenu.scss'
+
 const MobileMenu = () => {
   return (
-    <div>MobileMenu</div>
+    <div className='menu'>
+      <div className="menu__inner">
+        <div className='menu__inner-books'>
+          <p>Витрина книг</p>
+        </div>
+        <div className='menu__inner-rules'>
+          <p>Правила пользования</p>
+        </div>
+        <div className='menu__inner-contract'>
+          <p>Договор оферты</p>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import MobileMenu from '../MobileMenu/MobileMenu'
 import './navbar.scss'
 import Menu from '../../assets/icons/Menu.svg'
+import Close from '../../assets/icons/Close.svg'
 import ShoppingCart from '../../assets/icons/shoppingCart.svg'
 
 const Navbar = () => {
@@ -12,20 +13,23 @@ const Navbar = () => {
   }
 
   return (
-    <div className='header'>
-      <div className="header__inner">
-        <div className="header__inner-left">
-          <div className="mobile-menu">
-            <img src={Menu} alt="" onClick={handleClick}/>
+    <>
+      <div className='header'>
+        <div className="header__inner">
+          <div className="header__inner-left">
+            <div className="mobile-menu">
+              <img src={open ? Close : Menu} alt="" onClick={handleClick} />
+            </div>
+            <a href="/" className='header__inner-logo'>Logo</a>
+            <a href="/" className='header__inner-name'>Библиотека</a>
           </div>
-          <a href="/" className='header__inner-logo'>Logo</a>
-          <a href="/" className='header__inner-name'>Библиотека</a>
-        </div>
-        <div className="header__inner-right">
-          <img src={ShoppingCart} alt="" />
+          <div className="header__inner-right">
+            <img src={ShoppingCart} alt="" />
+          </div>
         </div>
       </div>
-    </div>
+      {open && <MobileMenu />}
+    </>
   )
 }
 
